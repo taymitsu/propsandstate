b3764b90afbabfe37a5c309ac5f8a9ee567b0a4d
taymitsu <tmitsumori96@gmail.com>
2023-01-18 10:04:35 -0800

    Counter component added

---
+++ src/Counter.js
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+function Counter(props) {
+  const [count, setCount] = useState(6)
+
+  return (
+    <div className="Couunter">
+      <p>{props.label}</p>
+      <h1>{count}</h1>
+      <button onClick={ () => setCount(count + 1)}>+</button>
+      <button>-</button>
+
+    </div>
+  )
+}
+
+export default Counter 
